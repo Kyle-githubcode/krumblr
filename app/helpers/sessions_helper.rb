76e509ec0d65384be57d855c696044fb10fb0b2e
Kyle-githubcode <kylebfaulkner@hotmail.com>
2018-07-13 16:55:36 +0930

    Created navbar

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,5 +1,15 @@
 module SessionsHelper
+    #sets user id in session cookie
     def log_in(user)
         session[:user_id] = user.id
+    end
+
+    #finds user object based on the id in the session
+    def current_user
+        @current_user ||= User.find_by(id: session[:user_id])
+    end
+
+    def logged_in?
+        current_user.present?
     end
 end
